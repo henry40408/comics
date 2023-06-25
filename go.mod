c59a69e2a96bd49513175b82c583b276122863ff
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2023-06-25 22:47:08 +0800

    chore: initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/henry40408/comics
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"path/filepath"
+	"sort"
+)
+
+type Book struct {
+	Name  string
+	Pages []string
+}
+
+func ListBooks() ([]Book, error) {
+	var books []Book
+
+	err := filepath.Walk("data", func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.Name() == "data" {
+			return nil // ignore self
+		}
+
+		if info.IsDir() {
+			book := Book{
+				Name: info.Name(),
+			}
+
+			d := filepath.Join("data", info.Name())
+
+			var pages []string
+
+			err := filepath.Walk(d, func(path string, info fs.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+
+				if info.Name() == d {
+					return nil // ignore self
+				}
+
+				if !info.IsDir() {
+					pages = append(pages, filepath.Join(d, info.Name()))
+				}
+
+				return nil
+			})
+			if err != nil {
+				return err
+			}
+
+			sort.Strings(pages)
+			book.Pages = pages
+			books = append(books, book)
+			return nil
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	sort.Slice(books, func(i, j int) bool {
+		return books[i].Name > books[j].Name
+	})
+
+	return books, nil
+}
+
+func main() {
+	books, err := ListBooks()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v", books)
+}
