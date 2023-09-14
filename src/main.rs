1a6a07c12bca53b6f69fd0862a387818d2a59938
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2023-09-14 21:13:20 +0800

    chore: env logger

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{ffi::OsString, fs, path::Path};
 
 use clap::{Parser, Subcommand};
+use log::debug;
 
 const WATER_CSS: &'static str = include_str!("../assets/water.css");
 
@@ -27,6 +28,12 @@ struct Page {
     name: String,
 }
 
+impl Page {
+    fn is_valid(&self) -> bool {
+        return !self.path.is_empty() && !self.name.is_empty();
+    }
+}
+
 #[derive(Debug)]
 struct Book {
     path: String,
@@ -34,20 +41,47 @@ struct Book {
     pages: Vec<Page>,
 }
 
+impl Book {
+    fn is_valid(&self) -> bool {
+        return !self.path.is_empty() && !self.path.is_empty() && !self.pages.is_empty();
+    }
+}
+
 fn list_pages<P: AsRef<Path>>(path: P) -> std::io::Result<Vec<Page>> {
+    debug!("scan {}", &path.as_ref().to_string_lossy());
     let mut pages: Vec<Page> = fs::read_dir(&path)?
         .into_iter()
-        .filter_map(Result::ok)
-        .filter(|e| e.path().is_file())
+        .filter_map(|e| {
+            if e.is_err() {
+                debug!("skip a file {:?}", e)
+            }
+            Result::ok(e)
+        })
+        .filter(|e| match e.path().is_file() {
+            true => {
+                debug!("found a file {}", e.path().to_string_lossy());
+                true
+            }
+            false => {
+                debug!("skip a non-file {}", e.path().to_string_lossy());
+                false
+            }
+        })
         .filter(|e| match infer::get_from_path(e.path()) {
-            Ok(Some(inferred)) => inferred.matcher_type() == infer::MatcherType::Image,
-            _ => false,
+            Ok(Some(inferred)) => {
+                debug!("found an image {}", e.path().to_string_lossy());
+                inferred.matcher_type() == infer::MatcherType::Image
+            }
+            _ => {
+                debug!("skip a non-image {}", e.path().to_string_lossy());
+                false
+            }
         })
         .map(|entry| Page {
             name: entry.file_name().to_string_lossy().to_string(),
             path: entry.path().to_string_lossy().to_string(),
         })
-        .filter(|p| !p.path.is_empty())
+        .filter(Page::is_valid)
         .collect();
     pages.sort_by(|a, b| a.path.cmp(&b.path));
     Ok(pages)
@@ -56,14 +90,32 @@ fn list_pages<P: AsRef<Path>>(path: P) -> std::io::Result<Vec<Page>> {
 fn list_books<P: AsRef<Path>>(path: P) -> std::io::Result<Vec<Book>> {
     let mut books: Vec<Book> = fs::read_dir(&path)?
         .into_iter()
-        .filter_map(Result::ok)
-        .filter(|e| e.path().is_dir())
+        .filter_map(|e| {
+            if e.is_err() {
+                debug!("skip {:?}", e);
+            }
+            Result::ok(e)
+        })
+        .filter(|e| match e.path().is_dir() {
+            true => {
+                debug!("find a directory {}", e.path().to_string_lossy());
+                true
+            }
+            false => {
+                debug!("skip a non-directory {}", e.path().to_string_lossy());
+                false
+            }
+        })
         .map(|entry| Book {
             name: entry.file_name().to_string_lossy().to_string(),
             pages: list_pages(entry.path()).unwrap_or(vec![]),
             path: entry.path().to_string_lossy().to_string(),
         })
-        .filter(|b| !b.path.is_empty())
+        .map(|b| {
+            debug!("found a book {} ({}P)", &b.name, &b.pages.len());
+            b
+        })
+        .filter(Book::is_valid)
         .collect();
     books.sort_by(|a, b| a.path.cmp(&b.path));
     Ok(books)
@@ -71,16 +123,14 @@ fn list_books<P: AsRef<Path>>(path: P) -> std::io::Result<Vec<Book>> {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    env_logger::init();
     let cli = Cli::parse();
     match &cli.command {
         Some(Commands::List { .. }) => {
             let data_dir = cli.data_dir.unwrap_or(OsString::from("./data"));
             let books = list_books(data_dir)?;
             for book in &books {
-                println!("Book: {}", book.path);
-                for page in &book.pages {
-                    println!("Page: {}", page.path);
-                }
+                println!("{} ({}P)", book.name, book.pages.len());
             }
             println!("{} book(s)", books.len());
         }
