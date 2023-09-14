f176795da27e8fd1fbf480d55c7378e274263411
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2023-09-14 23:26:46 +0800

    feat: serve

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
-use std::{ffi::OsString, fs, path::Path};
-
+use axum::{routing::get, Router};
 use clap::{Parser, Subcommand};
-use log::debug;
+use std::{ffi::OsString, fs, net::SocketAddr, path::Path};
+use tower_http::{
+    services::ServeDir,
+    trace::{self, TraceLayer},
+};
+use tracing::{debug, info, Level};
+use tracing_subscriber::EnvFilter;
 
 const WATER_CSS: &'static str = include_str!("../assets/water.css");
 
 #[derive(Parser, Debug)]
 #[command(author, version, about,long_about=None)]
 struct Cli {
+    /// Debug mode
+    #[arg(long, short = 'd')]
+    debug: bool,
+
     /// Data directory
     #[arg(long)]
     data_dir: Option<OsString>,
@@ -19,7 +28,15 @@ struct Cli {
 #[derive(Subcommand, Debug)]
 enum Commands {
     /// List books
+    #[command(alias = "ls")]
     List {},
+    /// Serve books
+    #[command(alias = "s")]
+    Serve {
+        /// Bind host & port
+        #[arg(long, short = 'b', default_value = "127.0.0.1:8080")]
+        bind: String,
+    },
 }
 
 #[derive(Debug)]
@@ -121,10 +138,37 @@ fn list_books<P: AsRef<Path>>(path: P) -> std::io::Result<Vec<Book>> {
     Ok(books)
 }
 
+async fn run_server(addr: SocketAddr) {
+    let app = Router::new()
+        .route("/healthz", get(|| async { "" }))
+        .nest_service("/assets", ServeDir::new("assets"))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+        );
+    info!("running on {:?}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .expect("failed to run the server");
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    env_logger::init();
     let cli = Cli::parse();
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(match cli.debug {
+                    true => Level::DEBUG.into(),
+                    false => Level::INFO.into(),
+                })
+                .from_env_lossy(),
+        )
+        .with_target(false)
+        .compact()
+        .init();
     match &cli.command {
         Some(Commands::List { .. }) => {
             let data_dir = cli.data_dir.unwrap_or(OsString::from("./data"));
@@ -133,6 +177,10 @@ async fn main() -> anyhow::Result<()> {
                 println!("{} ({}P)", book.name, book.pages.len());
             }
             println!("{} book(s)", books.len());
+        }
+        Some(Commands::Serve { bind }) => {
+            let bind: SocketAddr = bind.parse().expect("invalid host:port pair");
+            run_server(bind).await
         }
         None => {}
     };
