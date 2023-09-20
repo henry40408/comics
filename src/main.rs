1efac093c557c2c38d5cf1254503d1749e181a4e
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2023-09-20 21:57:35 +0800

    chore: lints

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,78 @@
-#![deny(clippy::pedantic, clippy::perf)]
+// https://github.com/EmbarkStudios/rust-ecosystem/blob/732513edfd9172f4eda358b2d0cefc6cad1585ee/lints.rs
+#![deny(unsafe_code)]
+#![warn(
+    clippy::all,
+    clippy::await_holding_lock,
+    clippy::char_lit_as_u8,
+    clippy::checked_conversions,
+    clippy::dbg_macro,
+    clippy::debug_assert_with_mut_call,
+    clippy::doc_markdown,
+    clippy::empty_enum,
+    clippy::enum_glob_use,
+    clippy::exit,
+    clippy::expl_impl_clone_on_copy,
+    clippy::explicit_deref_methods,
+    clippy::explicit_into_iter_loop,
+    clippy::fallible_impl_from,
+    clippy::filter_map_next,
+    clippy::flat_map_option,
+    clippy::float_cmp_const,
+    clippy::fn_params_excessive_bools,
+    clippy::from_iter_instead_of_collect,
+    clippy::if_let_mutex,
+    clippy::implicit_clone,
+    clippy::imprecise_flops,
+    clippy::inefficient_to_string,
+    clippy::invalid_upcast_comparisons,
+    clippy::large_digit_groups,
+    clippy::large_stack_arrays,
+    clippy::large_types_passed_by_value,
+    clippy::let_unit_value,
+    clippy::linkedlist,
+    clippy::lossy_float_literal,
+    clippy::macro_use_imports,
+    clippy::manual_ok_or,
+    clippy::map_err_ignore,
+    clippy::map_flatten,
+    clippy::map_unwrap_or,
+    clippy::match_on_vec_items,
+    clippy::match_same_arms,
+    clippy::match_wild_err_arm,
+    clippy::match_wildcard_for_single_variants,
+    clippy::mem_forget,
+    clippy::mismatched_target_os,
+    clippy::missing_enforced_import_renames,
+    clippy::mut_mut,
+    clippy::mutex_integer,
+    clippy::needless_borrow,
+    clippy::needless_continue,
+    clippy::needless_for_each,
+    clippy::option_option,
+    clippy::path_buf_push_overwrite,
+    clippy::ptr_as_ptr,
+    clippy::rc_mutex,
+    clippy::ref_option_ref,
+    clippy::rest_pat_in_fully_bound_structs,
+    clippy::same_functions_in_if_condition,
+    clippy::semicolon_if_nothing_returned,
+    clippy::single_match_else,
+    clippy::string_add,
+    clippy::string_add_assign,
+    clippy::string_lit_as_bytes,
+    clippy::string_to_string,
+    clippy::todo,
+    clippy::trait_duplication_in_bounds,
+    clippy::unimplemented,
+    clippy::unnested_or_patterns,
+    clippy::unused_self,
+    clippy::useless_transmute,
+    clippy::verbose_file_reads,
+    clippy::zero_sized_map_values,
+    future_incompatible,
+    nonstandard_style,
+    rust_2018_idioms
+)]
 
 use askama::Template;
 use axum::{
@@ -36,11 +110,11 @@ const WATER_CSS: &str = include_str!("../assets/water.css");
 #[command(author, version, about, long_about=None, arg_required_else_help(true))]
 struct Cli {
     /// Debug mode
-    #[arg(long, short = 'd')]
+    #[arg(long, short = 'd', env = "DEBUG")]
     debug: bool,
 
     /// Data directory
-    #[arg(long)]
+    #[arg(long, env = "DATA_DIR")]
     data_dir: Option<OsString>,
 
     #[command(subcommand)]
@@ -59,7 +133,7 @@ enum Commands {
     #[command(alias = "s")]
     Serve {
         /// Bind host & port
-        #[arg(long, short = 'b', default_value = "127.0.0.1:8080")]
+        #[arg(long, short = 'b', env = "BIND", default_value = "127.0.0.1:8080")]
         bind: String,
     },
 }
@@ -347,7 +421,6 @@ async fn auth_middleware_fn<B>(request: Request<B>, next: Next<B>) -> Result<Res
     }
 }
 
-#[allow(clippy::unused_async)]
 async fn index_route(State(state): State<AppState>) -> impl IntoResponse {
     state
         .scan
@@ -381,7 +454,6 @@ async fn index_route(State(state): State<AppState>) -> impl IntoResponse {
         )
 }
 
-#[allow(clippy::unused_async)]
 async fn show_book_route(
     State(state): State<AppState>,
     query: Query<BookQuery>,
@@ -419,7 +491,6 @@ async fn show_book_route(
         )
 }
 
-#[allow(clippy::unused_async)]
 async fn rescan_books_route(State(state): State<AppState>) -> impl IntoResponse {
     state.scan.lock().map_or(Redirect::to("/"), |mut scan| {
         scan_books(&scan.data_dir)
@@ -447,7 +518,6 @@ struct DataQuery {
     id: String,
 }
 
-#[allow(clippy::unused_async)]
 async fn show_page_route(
     State(state): State<AppState>,
     query: Query<DataQuery>,
@@ -456,7 +526,7 @@ async fn show_page_route(
         .scan
         .lock()
         .map_err(|e| {
-            error!("failed to render page {e:?}");
+            debug!("failed to render page {e:?}");
             e
         })
         .ok()
@@ -468,7 +538,7 @@ async fn show_page_route(
                     .and_then(|p| {
                         fs::read(&p.path)
                             .map_err(|e| {
-                                error!("failed to read page {e:?}");
+                                debug!("failed to read page {e:?}");
                                 e
                             })
                             .ok()
