221e313f2f69ca4691e3205bc4c1070cdc989f8f
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2025-01-21 00:10:47 +0800

    chore: cargo update

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -5,7 +5,7 @@ use tempdir::TempDir;
 #[test]
 fn initial_scan_finished() {
     let mut cmd = Command::cargo_bin("comics").unwrap();
-    cmd.args(["--bind", "127.0.0.1:3000", "--data-dir", "fixtures/data"]);
+    cmd.args(["--bind", "127.0.0.1:0", "--data-dir", "fixtures/data"]);
     cmd.timeout(Duration::from_millis(100));
     cmd.assert()
         .stdout(predicates::str::contains("initial scan finished"));
@@ -17,7 +17,7 @@ fn initial_scan_failed() {
     let mut cmd = Command::cargo_bin("comics").unwrap();
     let non_exist = dir.path().join("non_exist");
     let path = non_exist.to_string_lossy();
-    cmd.args(["--bind", "127.0.0.1:3001", "--data-dir", &path]);
+    cmd.args(["--bind", "127.0.0.1:0", "--data-dir", &path]);
     cmd.timeout(Duration::from_millis(100));
     cmd.assert()
         .stdout(predicates::str::contains("initial scan failed"));
